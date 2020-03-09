1b919e587d9f48930763741f2c61eca06045ca7d
ResidenteDesarrolloWebIE <residente.desarrolloweb@integracion-energia.com>
2020-03-09 10:50:54 -0600

    Nuevo Diselo SCAFT logo

--- resources/views/layouts/partials/_navigationBar.blade.php
+++ resources/views/layouts/partials/_navigationBar.blade.php
@@ -7,7 +7,7 @@
             <span class="navbar-toggler-icon" ></span>
         </button>
         <a href="{{url('/home')}}" class="site-logo text-center">
-            <img class="logo" src="{{ asset('images/scaft4.png') }}" alt="">
+            <img class="logo" src="{{ asset('images/scaft6.png') }}" alt="">
         </a>
         <div class="collapse navbar-collapse collapse offset-md-3" id="navbarToggler">
             <ul class="nav navbar-nav" id="menuprincipal">
