1f27b1c89a49888d10e0461db29212f6ba85d4b6
Moises <163190@idsupchiapas.edu.mx>
2020-03-17 08:21:51 -0600

    Merge

--- resources/views/client/customers/create.blade.php
+++ resources/views/client/customers/create.blade.php
@@ -2,7 +2,6 @@
     <div class="modal-dialog modal-lg">
         <div class="modal-content">
             <span type="hidden" name="_token" value="{{{ csrf_token() }}}" id="tokenLoadImages"> </span>
-            <!-- Modal Header -->
             <div class="modal-header">
                 <h4 class="modal-title" id="idFolio"><span class="fa fa-spinner" aria-hidden="true"></span>&nbsp;&nbsp;<strong class="modal-folio">CREAR NUEVO CONTACTO</strong></h4>
                 <button type="button" class="" data-dismiss="modal">&times;</button>
@@ -10,7 +9,6 @@
             <div class="modal-body">
                 <form>
                     <div class="row">
-
                         <div class="form-group text-center">
                             <label for="formGroupExampleInput2"><strong style="color:red">*</strong><strong>Fecha de cotizacion</strong></label>
                             <input type="text" class="form-control" name="quotation_estimateddate" id="quotation_estimateddate" value="" required>
